d5f2ce26dd7802b7577f96f9a8d3f60443564a7f
Guilhermy <guilhermy.aquino@estudante.iftm.edu.br>
2025-11-22 17:57:53 -0300

    Adicionar propriedades da aplicação

--- src/main/java/web/controle_estoque/ControleVacinacaoApplication.java
+++ src/main/java/web/controle_estoque/ControleVacinacaoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ControleEstoqueApplication {
+public class ControleVacinacaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControleEstoqueApplication.class, args);
+		SpringApplication.run(ControleVacinacaoApplication.class, args);
 	}
 
 }
